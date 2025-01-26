11e07ee75de5be39ac9388f64eb75b8e450b294d
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2025-01-26 18:57:36 -0500

    fixed sql to create table

--- src/DataBase/DbContext.cs
+++ src/DataBase/DbContext.cs
@@ -21,7 +21,7 @@ namespace WebApi.Data
         {
             var sql = @"
                 CREATE TABLE IF NOT EXISTS ConfigurationItems (
-                    Key NCHAR(55) PRIMARY KEY NOT NULL UNIQUE,
+                    Key NCHAR(55) PRIMARY KEY,
                     Value VARCHAR(255) NOT NULL
                 );
             ";
